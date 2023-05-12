efa0d70546df0423cbef97bc5f6b3d9a858f745b
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-12 18:44:17 +0530

    Configure useRegisterModal using zustand

---
+++ src/components/Modals/RegisterModal.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { FC } from "react";
+
+interface RegisterModalProps {}
+
+const RegisterModal: FC<RegisterModalProps> = () => {
+  return <></>;
+};
+
+export default RegisterModal;
