67cf79239a0c1d0c4362d2bd4a58a703bd5b0e95
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-13 21:59:18 +0530

    Configure debug according to the environment

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -8,46 +8,53 @@ import prisma from "@/libs/prismadb";
 import argon2 from "argon2";
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string
-        }),
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                email: {
-                    label: "email",
-                    type: "text",
-                },
-                password: {
-                    label: "password",
-                    type: "password",
-                }
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error("Invalid Credentials")
-                }
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                });
-                if (!user || !user?.hashedPassword) {
-                    throw new Error("Invalid Credentials");
-                }
-                const isValidPassword = await argon2.verify(user.hashedPassword, credentials.password);
-                if (!isValidPassword) {
-                    throw new Error("Invalid Credentials");
-                }
-                return user;
-            }
-        }),
-    ]
-} 
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+        },
+        password: {
+          label: "password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid Credentials");
+        }
+        const isValidPassword = await argon2.verify(
+          user.hashedPassword,
+          credentials.password
+        );
+        if (!isValidPassword) {
+          throw new Error("Invalid Credentials");
+        }
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+};
