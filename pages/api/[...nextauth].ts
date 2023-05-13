0f6157e167de64c7eab771d2a9ea51c90f158dc6
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-13 21:56:45 +0530

    Create Auth Providers and authorize function

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,53 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import prisma from "@/libs/prismadb";
+import argon2 from "argon2";
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: {
+                    label: "email",
+                    type: "text",
+                },
+                password: {
+                    label: "password",
+                    type: "password",
+                }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("Invalid Credentials")
+                }
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+                if (!user || !user?.hashedPassword) {
+                    throw new Error("Invalid Credentials");
+                }
+                const isValidPassword = await argon2.verify(user.hashedPassword, credentials.password);
+                if (!isValidPassword) {
+                    throw new Error("Invalid Credentials");
+                }
+                return user;
+            }
+        }),
+    ]
+} 
