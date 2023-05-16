f0b97e8a22f48bb9730e6a2b20b707db7d519214
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-16 13:58:18 +0530

    Add action labels and actions

--- src/components/Modals/RentModal.tsx
+++ src/components/Modals/RentModal.tsx
@@ -1,10 +1,44 @@
 "use client";
 
+import { useMemo, useState } from "react";
 import useRentModal from "@/hooks/useRentModal";
 import Modal from "./Modal";
 
+const enum STEPS {
+  CATEGORY = 1,
+  LOCATION = 2,
+  INFO = 3,
+  IMAGES = 4,
+  DESCRIPTION = 5,
+  PRICE = 5,
+}
+
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const onPrev = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+    return "Back";
+  }, [step]);
 
   return (
     <Modal
@@ -12,7 +46,9 @@ const RentModal = () => {
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
       onSubmit={rentModal.onClose}
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? onPrev : undefined}
     />
   );
 };
