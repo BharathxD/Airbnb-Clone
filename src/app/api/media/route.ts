965a25b045accbbd659948a910d267a1b0af0500
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-18 13:46:19 +0530

    Create a full public object URL

--- src/app/api/media/route.ts
+++ src/app/api/media/route.ts
@@ -4,36 +4,38 @@ import { StatusCodes } from "http-status-codes";
 import { NextRequest, NextResponse } from "next/server";
 
 type Data = {
-    s3UploadUrl: string;
-    key: string;
+  s3UploadUrl: string;
+  fullUrl: string;
 };
 
 export async function GET(req: NextRequest, res: NextResponse) {
-    try {
-        const extension = req.url?.split("%2F")[1];
-        const key = `picture_${randomUUID()}.${extension}`;
-        const s3Params = {
-            Bucket: process.env.AWS_S3_BUCKET_NAME,
-            Key: key,
-            Expires: 10,
-            ContentType: `image/${extension}`,
-        };
-        const s3UploadUrl = await s3.getSignedUrlPromise("putObject", s3Params);
-        const data: Data = { s3UploadUrl, key };
-        const responseBody = JSON.stringify(data);
+  try {
+    const extension = req.url?.split("%2F")[1];
+    const key = `picture_${randomUUID()}.${extension}`;
+    const s3Params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: key,
+      Expires: 10,
+      ContentType: `image/${extension}`,
+    };
+    const s3UploadUrl = await s3.getSignedUrlPromise("putObject", s3Params);
+    const bucketUrl = `https://s3.amazonaws.com/${process.env.AWS_S3_BUCKET_NAME}`;
+    const fullUrl = `${bucketUrl}/${key}`;
+    const data: Data = { s3UploadUrl, fullUrl };
+    const responseBody = JSON.stringify(data);
 
-        return new NextResponse(responseBody, {
-            status: StatusCodes.OK,
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    } catch (error: unknown) {
-        console.error(
-            `Failed to generate presigned URL: ${(error as Error).message}`
-        );
-        return new NextResponse("Failed to generate signed URL", {
-            status: StatusCodes.INTERNAL_SERVER_ERROR,
-        });
-    }
+    return new NextResponse(responseBody, {
+      status: StatusCodes.OK,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error: unknown) {
+    console.error(
+      `Failed to generate presigned URL: ${(error as Error).message}`
+    );
+    return new NextResponse("Failed to generate signed URL", {
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
+    });
+  }
 }
