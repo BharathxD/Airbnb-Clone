ad9d8a075523cebad924821911a3f4975e170aea
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-18 12:50:44 +0530

    Add styling to the Dropzone

--- src/app/api/media/route.ts
+++ src/app/api/media/route.ts
@@ -7,27 +7,26 @@ import { NextResponse } from "next/server";
 import { StatusCodes } from "http-status-codes";
 
 type Data = {
-    s3UploadUrl: string;
-    key: string;
-}
+  s3UploadUrl: string;
+  key: string;
+};
 
-export async function GET(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    try {
-        const extension = ((req.query.fileType as string).split("/", 1));
-        const key = `picture_${randomUUID()}.ext`;
-        const s3Params = {
-            Bucket: process.env.BUCKET_NAME,
-            key,
-            Expires: 10,
-            ContentType: `image/${extension}`,
-        }
-        const s3UploadUrl = await s3.getSignedUrlPromise("putObject", s3Params);
-        return res.status(StatusCodes.OK).json({ s3UploadUrl, key: key })
-    } catch (error: any) {
-        console.log(`Failed to generate presigned url: ${error.message}`);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Failed to Generate Signed URL");
-    }
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const extension = (req.query.fileType as string).split("/", 1);
+    const key = `picture_${randomUUID()}.ext`;
+    const s3Params = {
+      Bucket: process.env.BUCKET_NAME,
+      key,
+      Expires: 10,
+      ContentType: `image/${extension}`,
+    };
+    const s3UploadUrl = await s3.getSignedUrlPromise("putObject", s3Params);
+    return res.status(StatusCodes.OK).json({ s3UploadUrl, key: key });
+  } catch (error: any) {
+    console.log(`Failed to generate presigned url: ${error.message}`);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send("Failed to Generate Signed URL");
+  }
 }
--- src/components/Inputs/ImageUpload.tsx
+++ src/components/Inputs/ImageUpload.tsx
@@ -1,11 +1,30 @@
 import { FC } from "react";
+import Dropzone, { useDropzone, Accept } from "react-dropzone";
+import { TbPhotoPlus } from "react-icons/tb";
 
 interface ImageUploadProps {}
 
 const ImageUpload: FC<ImageUploadProps> = () => {
-  return <div>
-
-  </div>;
+  const { getRootProps, getInputProps, open } = useDropzone();
+  const acceptedFileTypes: Accept = {
+    "image/jpeg": [".jpg", ".jpeg"],
+    "image/png": [".png"],
+  };
+  return (
+    <div>
+      <Dropzone accept={acceptedFileTypes}>
+        {({ getRootProps, getInputProps }) => (
+          <div
+            {...getRootProps()}
+            className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 rounded-lg"
+          >
+            <input {...getInputProps()} />
+            <TbPhotoPlus />
+          </div>
+        )}
+      </Dropzone>
+    </div>
+  );
 };
 
 export default ImageUpload;
