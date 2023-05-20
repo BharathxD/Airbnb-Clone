d9c590a8b6a2fc17b5498872990b0d977ac150a2
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-20 21:39:46 +0530

    Create action function to fetch all the listings

--- src/components/UI/EmptyState.tsx
+++ src/components/UI/EmptyState.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import { FC } from "react";
 import Heading from "./Heading";
 import Button from "./Button";
-import { Router } from "next/router";
 
 interface EmptyStateProps {
   title?: string;
