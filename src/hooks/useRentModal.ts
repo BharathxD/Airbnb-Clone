f7370cbfdb1825308cae41a4a417105fbf8c048f
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-23 23:18:03 +0530

    Generate documentation for rentModal

--- src/hooks/useRentModal.ts
+++ src/hooks/useRentModal.ts
@@ -6,7 +6,9 @@ interface useRentModalStore {
   onClose: () => void;
 }
 
-
+/* This code is creating a custom hook called `useRentModal` using the `create` function from the
+`zustand` library. The hook returns an object with three properties: `isOpen`, `onOpen`, and
+`onClose`. */
 const useRentModal = create<useRentModalStore>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
