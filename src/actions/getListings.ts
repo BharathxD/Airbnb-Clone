894dae29f6d9438c6db26fe1345e70aa9c4e272e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-27 14:42:58 +0530

    Refactor: Refactor getListings function to query listings based on the specified parameters

--- src/actions/getListings.ts
+++ src/actions/getListings.ts
@@ -1,24 +1,45 @@
 "use server";
 
 import prisma from "@/libs/prismadb";
-import { Listing } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 export interface IListingParams {
-  userId: Listing["id"];
+  userId?: User["id"];
+  guestCount?: Listing["guestCount"];
+  roomCount?: Listing["roomCount"];
+  bathroomCount?: Listing["bathroomCount"];
+  startDate?: Reservation["startDate"];
+  endDate?: Reservation["endDate"];
+  locationValue?: Listing["locationValue"];
+  category?: string;
 }
 
 const getListings = async (params: IListingParams) => {
   try {
-    const { userId } = params;
-    const query = userId ? { userId } : {};
+    // Extract the parameters from the input object
+    const { userId, guestCount, roomCount, bathroomCount, startDate, endDate, locationValue, category } = params;
+
+    // Construct the query object to be used in the Prisma query
+    const query: any = {
+      ...(userId && { userId }),
+      ...(category && { category }),
+      ...(roomCount && { roomCount: { gte: +roomCount } }),
+      ...(guestCount && { guestCount: { gte: +guestCount } }),
+      ...(bathroomCount && { bathroomCount: { gte: +bathroomCount } }),
+      ...(locationValue && { locationValue }),
+    };
+
+    // Retrieve the listings from the database based on the constructed query
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: { createdAt: "desc" },
     });
+
     const safeListings = listings.map((listing) => ({
       ...listing,
       createdAt: listing.createdAt.toISOString(),
     }));
+
     return safeListings;
   } catch (error: any) {
     throw new Error(error);
