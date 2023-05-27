da60947356ec4fabbeab4680ff986670fdb1bab0
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-27 20:47:59 +0530

    Change environment variables as they are reserved for Vercel Deployment

--- aws/s3/s3.ts
+++ aws/s3/s3.ts
@@ -1,10 +1,10 @@
 import S3 from "aws-sdk/clients/s3";
 
 const s3 = new S3({
-  region: process.env.AWS_REGION,
+  region: process.env.NEXT_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY || "",
-    secretAccessKey: process.env.AWS_SECRET_KEY || "",
+    accessKeyId: process.env.NEXT_AWS_ACCESS_KEY || "",
+    secretAccessKey: process.env.NEXT_AWS_SECRET_KEY || "",
   },
   signatureVersion: "v4",
 });
--- src/app/api/media/route.ts
+++ src/app/api/media/route.ts
@@ -14,13 +14,13 @@ export async function GET(req: NextRequest, _: NextResponse) {
     const extension = req.url?.split("%2F")[1];
     const key = `picture_${randomUUID()}.${extension}`;
     const s3Params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
       Key: key,
       Expires: 10,
       ContentType: `image/${extension}`,
     };
     const s3UploadUrl = await s3.getSignedUrlPromise("putObject", s3Params);
-    const fullUrl = `${process.env.AWS_S3_ENDPOINT}/${key}`;
+    const fullUrl = `${process.env.NEXT_AWS_S3_ENDPOINT}/${key}`;
     const data: Data = { s3UploadUrl, fullUrl };
     const responseBody = JSON.stringify(data);
 
@@ -44,7 +44,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const key = req.url?.split("com%2F")[1];
     const BucketParams = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME || "",
+      Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME || "",
       Key: key,
     } satisfies DeleteObjectRequest;
     s3.deleteObject(BucketParams, function (err, data) {
