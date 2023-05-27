1a63398931612c0d9a473cca201b1d789466250a
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-27 22:48:10 +0530

    Implement error handling for ListingPage

--- src/app/listings/[listingid]/page.tsx
+++ src/app/listings/[listingid]/page.tsx
@@ -9,22 +9,45 @@ interface IParams {
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
-  const listing = await getListingById(params);
-  const currentUser = await getCurrentUser();
-  const reservations = await getReservation({ listingId: listing?.id });
-  if (!listing)
+  try {
+    if (!params || !params.listingId) {
+      return (
+        <EmptyState
+          title="Invalid listing ID"
+          subtitle="Please provide a valid listing ID"
+        />
+      );
+    }
+
+    const listing = await getListingById({ listingId: params.listingId });
+    const currentUser = await getCurrentUser();
+    const reservations = await getReservation({ listingId: listing?.id });
+
+    if (!listing) {
+      return (
+        <EmptyState
+          title="Something went wrong"
+          subtitle="We'll be back soon"
+        />
+      );
+    }
+
     return (
-      <EmptyState title="Something went wrong" subtitle="We'll be back soon" />
+      <div className="pt-[12vh]">
+        <ListingClient
+          listing={listing}
+          currentUser={currentUser}
+          reservations={reservations}
+        />
+      </div>
     );
-  return (
-    <div className="pt-[12vh]">
-      <ListingClient
-        listing={listing}
-        currentUser={currentUser}
-        reservations={reservations}
-      />
-    </div>
-  );
+  } catch (error) {
+    // Handle any errors that occur during fetching or rendering
+    console.error(error);
+    return (
+      <EmptyState title="Error occurred" subtitle="Please try again later" />
+    );
+  }
 };
 
 export default ListingPage;
