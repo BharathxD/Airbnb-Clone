5f1ae2587d345f2039da6bffb8d69d25c649657a
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-28 13:46:36 +0530

    Added a messages to let users know that this is a bug

--- src/app/listings/[listingid]/page.tsx
+++ src/app/listings/[listingid]/page.tsx
@@ -15,6 +15,14 @@ interface ISearchParams {
 const ListingPage = async ({ params }: ISearchParams) => {
   //? App in production, returning `undefined` for `listingId` parameter
   //? But in development it returns the `listingId`
+  if (!params || !params?.listingId) {
+    return (
+      <EmptyState
+        title="This is a known bug in Next 13.4.1"
+        subtitle="Every other feature works well. I will find a workaround as soon as I can."
+      />
+    );
+  }
   const listing = await getListingById({ listingId: params?.listingId });
   const currentUser = await getCurrentUser();
   const reservations = await getReservation({ listingId: listing?.id });
