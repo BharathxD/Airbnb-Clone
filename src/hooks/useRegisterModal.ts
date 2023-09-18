335474f41453eeae2df40701597d607adda054f6
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-18 23:49:24 +0530

    feat: Add useRegisterModal Zustand store

--- src/hooks/useRegisterModal.ts
+++ src/hooks/useRegisterModal.ts
@@ -1,21 +1,49 @@
 import { create } from "zustand";
 
+/**
+ * Represents the state and methods for a registration modal.
+ *
+ * @interface useRegisterModalStore
+ * @property {boolean} isOpen - Indicates whether the registration modal is open.
+ * @property {() => void} onOpen - Function to open the registration modal.
+ * @property {() => void} onClose - Function to close the registration modal.
+ */
+
 interface useRegisterModalStore {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
 }
 
-/* This code is creating a custom hook called `useRegisterModal` using the `create` function from the
-`zustand` library. The hook returns an object with three properties: `isOpen`, `onOpen`, and
-`onClose`. `isOpen` is a boolean value that indicates whether the register modal is currently open
-or not. `onOpen` is a function that sets the `isOpen` property to `true`, and `onClose` is a
-function that sets the `isOpen` property to `false`. The `set` function is used to update the state
-of the `isOpen` property. The `useRegisterModal` hook can be used in any component to manage the
-state of the register modal. */
+/**
+ * Creates a Zustand store for managing the state of a registration modal.
+ *
+ * This custom hook, `useRegisterModal`, is created using the `create` function from the `zustand` library.
+ * It returns an object with three properties: `isOpen`, `onOpen`, and `onClose`.
+ *
+ * - `isOpen`: A boolean value that indicates whether the registration modal is open or not.
+ * - `onOpen`: A function to set the `isOpen` property to `true`, opening the registration modal.
+ * - `onClose`: A function to set the `isOpen` property to `false`, closing the registration modal.
+ *
+ * @function useRegisterModal
+ * @returns {useRegisterModalStore} - An object representing the state and methods for managing the registration modal.
+ */
+
 const useRegisterModal = create<useRegisterModalStore>((set) => ({
   isOpen: false,
+
+  /**
+   * Opens the registration modal.
+   *
+   * @function onOpen
+   */
   onOpen: () => set({ isOpen: true }),
+
+  /**
+   * Closes the registration modal.
+   *
+   * @function onClose
+   */
   onClose: () => set({ isOpen: false }),
 }));
 
